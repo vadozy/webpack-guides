849d0c4d17866738ad5cc1bb3acdc41185e41721
Vadim Storozhuk <vadozy@yahoo.com>
2020-05-02 01:14:07 -0400

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import IconWebpack from './icon-webpack.png';
+import Data from './data.xml';
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = IconWebpack;
+  element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,39 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: [
+          'csv-loader',
+        ],
+      },
+      {
+        test: /\.xml$/i,
+        use: [
+          'xml-loader',
+        ],
+      },
+    ],
   },
 };
