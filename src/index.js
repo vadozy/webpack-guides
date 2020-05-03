0091933ea02538567eaaa2d3712ac83ad4ca3dd8
Vadim Storozhuk <vadozy@yahoo.com>
2020-05-03 17:50:43 -0400

    Code Splitting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import _ from 'lodash';
-import printMe from './print';
-
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ 'lodash')
+    .then(({ default: _}) => {
+      const element = document.createElement('div');
+    
+      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    
+      return element;
+    })
+    .catch(err => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component());
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js',
+    index: './src/index.js',
   },
   plugins: [
     new CleanWebpackPlugin(),
