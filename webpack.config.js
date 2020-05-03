111966a74561f859de5d22c7da1fe888efda7c94
Vadim Storozhuk <vadozy@yahoo.com>
2020-05-03 00:00:18 -0400

    Development

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    open: 'Google Chrome',
+  },
   entry: {
     app: './src/index.js',
     print: './src/print.js',
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       title: 'Output Management',
     }),
@@ -16,5 +22,6 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
 };
